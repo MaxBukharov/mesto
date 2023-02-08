1d12bcf10bb707c99bae81109447584d9e25ee17
Max Bukharov <max.sherbinin2017@yandex.ru>
2023-02-08 18:30:58 +0300

    hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,20 @@ let profileName = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__subtitle');
 
 popupButton.addEventListener('click', function () {
-  popupContainer.classList.remove('popup__hidden');
+  popupContainer.classList.remove('popup_hidden');
   popupName.value = profileName.textContent;
   popupDescription.value = profileDescription.textContent;
 });
 
 popupCloseButton.addEventListener('click', function () {
-  popupContainer.classList.add('popup__hidden');
+  popupContainer.classList.add('popup_hidden');
 });
 
 function popupSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
-  popupContainer.classList.add('popup__hidden');
+  popupContainer.classList.add('popup_hidden');
 }
 
 popupForm.addEventListener('submit', popupSubmit);
