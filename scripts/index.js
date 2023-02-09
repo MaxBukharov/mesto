9df0fbfc412a1e3f9373f46e3bbf2a5eb4043e0b
Max Bukharov <max.sherbinin2017@yandex.ru>
2023-02-09 14:26:06 +0300

    hotfix review2

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ let editButton = document.querySelector('.profile__edit-button');
 let popupContainer = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__form');
 let popupCloseButton = document.querySelector('.popup__close');
-let popupName = popupForm.querySelector('.popup__form-input_name');
-let popupDescription = popupForm.querySelector('.popup__form-input_description');
+let popupName = popupForm.querySelector('.popup__form-input_profile_name');
+let popupDescription = popupForm.querySelector('.popup__form-input_profile_description');
 let profileName = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__subtitle');
 
@@ -14,14 +14,16 @@ function popupSubmit(evt) {
   popupContainer.classList.add('popup_hidden');
 }
 
+function popupClose() {
+  popupContainer.classList.add('popup_hidden');
+}
+
 editButton.addEventListener('click', function () {
   popupContainer.classList.remove('popup_hidden');
   popupName.value = profileName.textContent;
   popupDescription.value = profileDescription.textContent;
 });
 
-popupCloseButton.addEventListener('click', function () {
-  popupContainer.classList.add('popup_hidden');
-});
+popupCloseButton.addEventListener('click', popupClose);
 
 popupForm.addEventListener('submit', popupSubmit);
