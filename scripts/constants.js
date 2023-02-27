a206cb731ee91e3287acb8ee7d5e8082435b8faa
Max Bukharov <max.sherbinin2017@yandex.ru>
2023-02-27 23:14:53 +0300

    Исправить ошибки после 1-го ревью

---
+++ scripts/constants.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    name: 'Сортавала',
+    src: 'https://images.unsplash.com/photo-1590079019458-0eb5b40a3371?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    alt: 'Поезд в лесу'
+  },
+  {
+    name: 'Карелия',
+    src: 'https://images.unsplash.com/photo-1548288242-d454d4648b55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80',
+    alt: 'Утка на причале'
+  },
+  {
+    name: 'Эльбрус',
+    src: 'https://images.unsplash.com/photo-1633400388065-e01e1f9cd4e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80',
+    alt: 'Горы'
+  },
+  {
+    name: 'Казань',
+    src: 'https://images.unsplash.com/photo-1584270692240-0411d322e4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80',
+    alt: 'Солнце и месяц'
+  },
+  {
+    name: 'Санкт-Петербург',
+    src: 'https://images.unsplash.com/photo-1619866328625-a70d35ec49db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
+    alt: 'Вид на Лахта-центр'
+  },
+  {
+    name: 'Рыбачий',
+    src: 'https://images.unsplash.com/photo-1663245360704-0d23d90c993c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    alt: 'Остров'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popupContainer = document.querySelector('.popup');
+const popupEditContainer = document.querySelector('.popup_edit-profile');
 const popupAddContainer = document.querySelector('.popup-add');
 const popupForm = document.querySelector('.popup__form');
 const popupAddForm = document.querySelector('.popup-add__form');
@@ -12,45 +12,16 @@ const popupCardImage = popupAddForm.querySelector('.popup-add__form-input_card_i
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__subtitle');
 const addButton = document.querySelector('.profile__add-button');
-const card = document.querySelector('#cardTemplate');
-const imagePopup = document.querySelector('.image-popup');
-const initialCards = [
-  {
-    name: 'Сортавала',
-    src: 'https://images.unsplash.com/photo-1590079019458-0eb5b40a3371?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    alt: 'Поезд в лесу'
-  },
-  {
-    name: 'Карелия',
-    src: 'https://images.unsplash.com/photo-1548288242-d454d4648b55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80',
-    alt: 'Утка на причале'
-  },
-  {
-    name: 'Эльбрус',
-    src: 'https://images.unsplash.com/photo-1633400388065-e01e1f9cd4e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80',
-    alt: 'Горы'
-  },
-  {
-    name: 'Казань',
-    src: 'https://images.unsplash.com/photo-1584270692240-0411d322e4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80',
-    alt: 'Солнце и месяц'
-  },
-  {
-    name: 'Санкт-Петербург',
-    src: 'https://images.unsplash.com/photo-1619866328625-a70d35ec49db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
-    alt: 'Вид на Лахта-центр'
-  },
-  {
-    name: 'Рыбачий',
-    src: 'https://images.unsplash.com/photo-1663245360704-0d23d90c993c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    alt: 'Остров'
-  }
-];
+const imagePopup = document.querySelector('.popup-image');
+const bigImage = imagePopup.querySelector('.image-popup__picture');
+const imageCaption = imagePopup.querySelector('.image-popup__caption');
+const imagePopupClose = imagePopup.querySelector('.image-popup__close');
+const cardTemplate = document.querySelector('#cardTemplate').content;
 
 const cardsGallery = document.querySelector('.elements');
 
 const createCard = card => {
-  const newCard = document.querySelector('#cardTemplate').content.cloneNode(true);
+  const newCard = cardTemplate.cloneNode(true);
   const cardName = newCard.querySelector('.element__title');
   const deleteButton = newCard.querySelector('.element__delete');
   const likeButton = newCard.querySelector('.element__like-button');
@@ -58,17 +29,11 @@ const createCard = card => {
   const cardImage = newCard.querySelector('.element__image');
   cardImage.setAttribute('src', card.src);
   cardImage.setAttribute('alt', card.alt);
-  cardsGallery.prepend(newCard);
   cardImage.addEventListener('click', function () {
     imagePopup.classList.add('image-popup_opened');
-    const bigImage = imagePopup.querySelector('.image-popup__picture');
     bigImage.setAttribute('src', cardImage.src);
-    const imageCaption = imagePopup.querySelector('.image-popup__caption');
     imageCaption.textContent = card.name;
-    const imagePopupClose = imagePopup.querySelector('.image-popup__close');
-    imagePopupClose.addEventListener('click', function () {
-      imagePopup.classList.remove('image-popup_opened');
-    });
+    imageCaption.setAttribute('alt', card.name);
   });
   deleteButton.addEventListener('click', function (evt) {
     evt.target.parentElement.remove();
@@ -76,13 +41,18 @@ const createCard = card => {
   likeButton.addEventListener('click', function (evt) {
     evt.target.classList.add('element__like-button_active');
   });
+  return newCard;
+};
+
+const addCard = card => {
+  cardsGallery.prepend(createCard(card));
 };
 
 function popupSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
-  popupClose();
+  popupClose(popupEditContainer);
 }
 
 function popupAddSubmit(evt) {
@@ -90,41 +60,43 @@ function popupAddSubmit(evt) {
   const card = new Object();
   card.name = popupCardName.value;
   card.src = popupCardImage.value;
-  initialCards.push(card);
-  createCard(card);
-  popupClose();
+  card.alt = popupCardName.value;
+  addCard(card);
+  popupClose(popupAddContainer);
 }
 
-function popupClose() {
-  popupContainer.classList.remove('popup_opened');
-  popupAddContainer.classList.remove('popup_opened');
+function popupClose(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-function popupOpen() {
-  popupContainer.classList.add('popup_opened');
+function popupOpen(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function imageDelete(card) {
-  initialCards.splice(card);
-}
-
-initialCards.forEach(createCard);
+initialCards.forEach(addCard);
 
 editButton.addEventListener('click', function () {
-  popupOpen();
+  popupOpen(popupEditContainer);
   popupName.value = profileName.textContent;
   popupDescription.value = profileDescription.textContent;
 });
 
 addButton.addEventListener('click', function () {
   popupAddContainer.classList.add('popup_opened');
-  popupCardName.value = '';
-  popupCardImage.value = '';
+  popupAddForm.reset();
 });
 
-popupCloseButton.addEventListener('click', popupClose);
+imagePopupClose.addEventListener('click', function () {
+  imagePopup.classList.remove('image-popup_opened');
+});
 
-popupAddCloseButton.addEventListener('click', popupClose);
+popupCloseButton.addEventListener('click', function () {
+  popupClose(popupEditContainer);
+});
+
+popupAddCloseButton.addEventListener('click', function () {
+  popupClose(popupAddContainer);
+});
 
 popupForm.addEventListener('submit', popupSubmit);
 
